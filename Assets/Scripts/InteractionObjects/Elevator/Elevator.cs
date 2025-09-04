1d3ef2dab462b0245b88f314e6771a4e19eb8c94
workantL <passant321@naver.com>
2025-09-04 15:44:50 +0900

    엘리베이터 코드 수정 중

---
+++ Assets/Scripts/InteractionObjects/Elevator/Elevator.cs
@@ -0,0 +1,65 @@
+using NUnit;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour,ISwitchable
+{
+    [SerializeField]
+    private Vector2 movePos;// 움직일 위치
+    [SerializeField]
+    private float moveDuration = 2f; // 이동하는데 걸리는 시간(기본값 : 2초)
+    private bool isActive; // 작동했는가?
+    public bool IsActive => isActive;
+
+
+    private bool isMoving; // 움직이고 있는 중인가
+
+    private Vector2 startPos; // 시작위치(엘리베이터 초기 위치)
+
+    private Animator animator2D;
+    private void Awake()
+    {
+        animator2D = GetComponentInChildren<Animator>();
+    }
+    private void OnEnable()
+    {
+        startPos = transform.position;// 초기 위치 설정
+    }
+    public void Active() // 위로 올라가기
+    {
+        if (isMoving) return;
+        isActive = true;
+        StartCoroutine(MoveTo(startPos, movePos));
+    }
+
+    public void Deactive() // 내려가기
+    {
+        if (isMoving) return;
+        isActive = false;
+        StartCoroutine(MoveTo(movePos, startPos));
+    }
+
+    private IEnumerator MoveTo(Vector2 start , Vector2 end)
+    {
+        isMoving = true;
+        if(animator2D != null)
+        {
+            animator2D.SetBool("IsMoving", true); // 애니메이터가 있다면 움직이는 애니메이션으로 변경
+        }
+        float elapsedTime = 0f;
+        while(elapsedTime < moveDuration)
+        {
+            float t = elapsedTime / moveDuration;
+            transform.position = Vector2.Lerp(start, end, t);
+            yield return null; // 다음 프레임까지 대기
+        }
+        transform.position = end;
+
+        if (animator2D != null) 
+        { 
+            animator2D.SetBool("IsMoving", false); 
+        }
+        isMoving = false;
+    }
+}
--- Assets/Unity6/Scripts/Object/Button.cs
+++ Assets/Unity6/Scripts/Object/Button.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Button : MonoBehaviour, IInteractable
 {
+    [SerializeField]
     public ISwitchable client; // ´ë»ó
     public void Interact()
     {
